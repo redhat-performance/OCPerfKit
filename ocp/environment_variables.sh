88f8a327bafecc48237a46f9c0b918eb2e5df7ee
Eli Battat <ebattat@redhat.com>
2024-12-10 18:25:34 +0200

    fix ods

--- ocp/environment_variables.sh
+++ ocp/environment_variables.sh
@@ -15,6 +15,8 @@ export LSO_VERSION_NAME="417"
 export ODF_DISK_MIN_SIZE="0.5Ti"
 export ODF_DISK_MAX_SIZE="2.5Ti"
 export ODF_DISK_PER_WORKER=3  # Maximum number of devices per node to be used
+# Total number of OSD
+export OSD_COUNT=9
 
 # WORKLOAD='stressng_pod', 'stressng_vm', 'stressng_kata', 'uperf_pod', 'uperf_vm', 'uperf_kata', 'hammerdb_pod_mariadb', 'hammerdb_vm_mariadb', 'hammerdb_kata_mariadb', 'hammerdb_pod_mariadb_lso', 'hammerdb_vm_mariadb_lso', 'hammerdb_kata_mariadb_lso', 'hammerdb_pod_postgres', 'hammerdb_vm_postgres', 'hammerdb_kata_postgres', 'hammerdb_pod_postgres_lso', 'hammerdb_vm_postgres_lso', 'hammerdb_kata_postgres_lso', 'hammerdb_pod_mssql', 'hammerdb_vm_mssql', 'hammerdb_kata_mssql', 'hammerdb_pod_mssql_lso', 'hammerdb_vm_mssql_lso', 'hammerdb_kata_mssql_lso', 'vdbench_pod', 'vdbench_kata', 'vdbench_vm', 'clusterbuster', 'bootstorm_vm', 'windows_vm'
 export WORKLOAD="hammerdb_vm_mssql"
