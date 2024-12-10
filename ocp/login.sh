122be5d0ae8fcdb6997b2488ddaa4a097e674216
Eli Battat <ebattat@redhat.com>
2024-12-11 00:18:02 +0200

    fix makefile

--- ocp/login.sh
+++ ocp/login.sh
@@ -1,4 +1,17 @@
-cp /home/azureuser/ocp/ocp4/auth/kubeconfig /home/azureuser/.kube/config
-cp /home/azureuser/ocp/ocp4/auth/kubeadmin-password /home/azureuser/.kube/kubeadmin-password
-oc login -u kubeadmin -p $(cat /home/azureuser/.kube/kubeadmin-password);
+#!/bin/bash
+# Set the main path
+HOME_PATH=$(dirname "$(pwd)")
+MAIN_PATH=$(pwd)
+echo "HOME_PATH=${HOME_PATH}"
+echo "MAIN_PATH=${MAIN_PATH}"
+
+# Create the .kube directory if it does not exist
+if [[ ! -d "${HOME_PATH}/.kube" ]]; then
+  echo "Creating directory ${HOME_PATH}/.kube/"
+  mkdir -p "${HOME_PATH}/.kube"
+fi
+
+cp ${HOME_PATH}/ocp4/auth/kubeconfig ${HOME_PATH}/.kube/config
+cp ${HOME_PATH}/ocp4/auth/kubeadmin-password ${HOME_PATH}/.kube/kubeadmin-password
+oc login -u kubeadmin -p $(cat ${HOME_PATH}/ocp4/auth/kubeadmin-password);
 oc get nodes;
